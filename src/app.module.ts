b79af54992e894603cd596f65c2e72d0856699b6
normyee <lunoqro@gmail.com>
2025-08-04 11:32:45 -0300

    feat: import dynamodb aws service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipeRepositoryDynamoDB } from './infra/aws/database/recipe.repository';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RecipeRepositoryDynamoDB],
 })
-export class AppModule {}
+export class AppModule { }
