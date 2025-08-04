ce2a4576693e48228e0433ffc45d8987a26fc5bf
normyee <lunoqro@gmail.com>
2025-08-04 11:32:19 -0300

    chore: add tables initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RecipeModel } from './infra/aws/database/Recipe.model';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  await RecipeModel.createTable();
+
 }
 bootstrap();
