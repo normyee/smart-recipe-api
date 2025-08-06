e24195e93a0e150cc77c30a7c135866fa19918f1
normyee <lunoqro@gmail.com>
2025-08-06 13:56:24 -0300

    chore: add all recipes columns for creation

--- src/infra/aws/database/recipe.repository.ts
+++ src/infra/aws/database/recipe.repository.ts
@@ -3,18 +3,49 @@ import { Injectable } from "@nestjs/common";
 import { RecipeRepository } from "src/entity/recipe.repository";
 import { dynamodb } from "../config";
 import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { Recipe } from "src/entity/Recipe";
+import { v4 as uuidv4 } from 'uuid';
+
+
+// {
+//   "title": "bolo de chocolate",
+//     "description": "um bolo simples e delicioso",
+//       "ingredients": [
+//         "2 ovos",
+//         "1 xícara de açúcar",
+//         "1/2 xícara de chocolate em pó",
+//         "1 xícara de farinha de trigo"
+//       ],
+//         "howToCook": "misture tudo e asse por 40 minutos a 180 graus",
+//           "image": "https://exemplo.com/imagem-bolo.jpg",
+//             "thumbnail": "https://exemplo.com/thumbnail-bolo.jpg",
+//               "author": {
+//     "name": "luan",
+//       "email": "luan@email.com"
+//   }
+// }
 
 
 @Injectable()
 export class RecipeRepositoryDynamoDB implements RecipeRepository {
-    async create(recipe: any) {
+    async create(recipe: Recipe) {
         try {
             const item = new PutItemCommand({
                 TableName: "recipes",
                 Item: {
-                    id: { S: "f4154c91-05e4-4234-9ccc-2b9aec96980e" },
-                    title: { S: "Sushi" },
-                    description: { S: "Uma receita fácil e prática de sushi, o gunkan leva apenas ovas de salmão (ikura), arroz e alga marinha tipo nori, nada mais." }
+                    id: { S: uuidv4() },
+                    title: { S: recipe.title },
+                    description: { S: recipe.description },
+                    ingredients: { L: recipe.ingredients.map(ingredient => ({ S: ingredient })) },
+                    howToCook: { S: recipe.howToCook },
+                    image: { S: recipe.image },
+                    thumbnail: { S: recipe.thumbnail },
+                    author: {
+                        M: {
+                            name: { S: recipe.author.name },
+                            email: { S: recipe.author.email }
+                        }
+                    }
                 }
             })
 
